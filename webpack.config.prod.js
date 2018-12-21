3c732ed594513062e0ae8119ac7d87ad3003c56d
Vishnu Baliga <vishnubaliga@Baligas-MacBook-Pro.local>
2018-12-21 20:24:17 +0530

    external CDNs for PROD

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,7 +1,15 @@
 const baseConfig = require("./webpack.config.base")
 const merge = require('webpack-merge')
-
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 
 module.exports = merge(baseConfig,{
-    mode: 'production'
+    mode: 'production',
+    plugins: [new BundleAnalyzerPlugin({
+        analyzerMode: 'static',
+        openAnalyzer: false
+    })],
+    externals: {
+        react : "React",
+        'react-dom': "ReactDOM"
+    }
 })
