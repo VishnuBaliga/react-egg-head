0e5b69135335a42cb972f1244699e765a437089b
Vishnu Baliga <vishnubaliga@Baligas-MacBook-Pro.local>
2018-12-21 18:13:03 +0530

    css-loader, react-hot-reload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import './style.css'
+import {hot} from 'react-hot-loader'
+
 
 class App extends React.Component{
-    render (){
-        return <h1>Hello Baliga is using react !!!</h1>
+
+    state = {
+        count : 0
+    }
+
+    render (){ 
+        return (
+            <div>
+                <h1>Hello Baliga is using react !!!</h1> 
+                <p>count: {this.state.count}</p>
+                <button onClick={()=>this.setState(state => ({count: state.count + 1}))}>+</button>
+                <button onClick={()=>this.setState(state => ({count: state.count - 1}))}>-</button>
+            </div>
+        )
     }
 }
 
-export default App
+export default hot(module)(App)
