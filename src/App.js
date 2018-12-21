3a295de7352c7809fb08475996f26cf3dd4eff68
Vishnu Baliga <vishnubaliga@Baligas-MacBook-Pro.local>
2018-12-21 21:21:09 +0530

    react suspense, react lazy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import './style.css'
-import {hot} from 'react-hot-loader'
+import '@babel/polyfill'
+import {hot} from 'react-hot-loader' 
 
+const Warning = React.lazy( () => import('./warning'))
 
 class App extends React.Component{
 
@@ -17,6 +19,13 @@ class App extends React.Component{
                 <p>count: {this.state.count}</p>
                 <button onClick={()=>this.setState(state => ({count: state.count + 1}))}>+</button>
                 <button onClick={()=>this.setState(state => ({count: state.count - 1}))}>-</button>
+
+            {this.state.count > 3 ?
+        <React.Suspense fallback={<p>Loading....</p>}>
+                <Warning />
+        </React.Suspense> : null
+        }
+
             </div>
         )
     }
---
+++ src/warning.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Warning extends React.Component{
+
+    render(){
+
+        return(
+            <div>warning!!!</div>
+        )
+    }
+}
+
+export default Warning
